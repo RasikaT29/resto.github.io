0d5449ef8663bbd8ddbca0bfa0b68ab67220de05
RasikaT29 <141399902+RasikaT29@users.noreply.github.com>
2023-08-04 14:24:09 +0530

    Add files via upload

---
+++ ras.js
@@ -0,0 +1,24 @@
+let food1s = document.getElementById('food1');
+let food2s = document.getElementById('food2');
+let food3s = document.getElementById('food3');
+let food4s = document.getElementById('food4');
+let food5s = document.getElementById('food5');
+let foods = document.getElementById('food');
+
+food1s.addEventListener('click'()= {
+     foods.style.backgroundImage="url ('res4.jpg')"
+})
+
+food2s.addEventListener('click'()= {
+    foods.style.backgroundImage="url ('ras.png')"
+})
+
+food3s.addEventListener('click'()= {
+    foods.style.backgroundImage="url ('res6.png')"
+})
+food4s.addEventListener('click'()= {
+    foods.style.backgroundImage="url ('food.jpg')"
+})
+food5s.addEventListener('click'()= {
+    foods.style.backgroundImage="url ('foo1.jpg')"
+})
